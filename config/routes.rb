13580fbab7351486177bcf77c00c0aea4224a9b8
Golovchenko Artem <golovchenkoart@gmail.com>
2017-12-16 14:25:46 +0200

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
+  #get 'welcom#index'
+  root 'welcom#index'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  #get 'welcom#index'
-  root 'welcom#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
